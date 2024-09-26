e44e8711a2b29bee85a9647e9fd7daec371e0dda
poojan2000 <pooja.niranjan1420@gmail.com>
2024-09-26 19:56:14 -0400

    My first commit

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Technical Project Manager",
+          "Scrum Master",
+          "Quality Engineer",
         ],
         autoStart: true,
         loop: true,
