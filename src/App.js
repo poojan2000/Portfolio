4753ce6968cb0451b82208a0dc29629a253b4051
poojan2000 <pooja.niranjan1420@gmail.com>
2024-10-19 00:01:28 -0400

    update

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/portfolio" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/portfolio"/>} />
+          <Route path="/Portfolio" element={<Home />} />
+          <Route path="/Portfolio/project" element={<Projects />} />
+          <Route path="/Portfolio/about" element={<About />} />
+          <Route path="/Portfolio/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/Portfolio"/>} />
         </Routes>
         <Footer />
       </div>
